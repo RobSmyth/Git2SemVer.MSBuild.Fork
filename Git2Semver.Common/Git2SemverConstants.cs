fa9902d8f5f4e7e965c192478e37634ac756a2ed
Rob Smyth <RobSmyth@users.noreply.github.com>
2024-10-27 08:42:27 +1100

    Rename generated ver files
    Renaming version info files

--- Git2Semver.Common/Git2SemverConstants.cs
+++ Git2Semver.Common/Git2SemverConstants.cs
@@ -7,9 +7,9 @@ public static class Git2SemverConstants
 {
     public const string DefaultScriptFilename = "Git2SemVer.csx";
 
-    public const string SharedVersionPropertiesFilename = "Git2SemVer.Generated.Properties.g.props";
+    public const string SharedVersionPropertiesFilename = "Git2SemVer.VersionInfo.g.props";
 
-    public const string SharedVersionJsonPropertiesFilename = "Git2SemVer.Generated.Properties.g.json";
+    public const string SharedVersionJsonPropertiesFilename = "Git2SemVer.VersionInfo.g.json";
 
     /// <summary>
     /// This folder name is hard coded in Directory.Versioning.Build.props.
