4b41663341954e310c7dec25d0946d11bf8adf7e
Rob Smyth <RobSmyth@users.noreply.github.com>
2024-11-23 15:34:23 +1100

    Cleanup

--- Git2SemVer.IntegrationTests/VersioningBuilds/CrossTargetingSolutionVersioningBuildTests.cs
+++ Git2SemVer.IntegrationTests/VersioningBuilds/CrossTargetingSolutionVersioningBuildTests.cs
@@ -4,72 +4,10 @@ namespace NoeticTools.Git2SemVer.IntegrationTests.VersioningBuilds;
 
 [TestFixture]
 [Parallelizable(ParallelScope.All)]
-internal class CrossTargetingSolutionVersioningBuildTests //: VersioningBuildTestsBase
+internal class CrossTargetingSolutionVersioningBuildTests : VersioningBuildTestsBase
 {
-    //protected override string SolutionFolderName => "CrossTargetingVersioning";
-
-    //protected override string SolutionName => "CrossTargetingVersioning.sln";
-    private VersioningBuildTestContext CreateTestContext()
+    protected override VersioningBuildTestContext CreateTestContext()
     {
         return new VersioningBuildTestContext("CrossTarget", "CrossTargetingTestSolution", "CrossTargetingVersioning.sln", "TestApplication");
-    }
-
-    [Test]
-    [CancelAfter(60000)]
-    public void BuildAndThenPackWithoutRebuildTest()
-    {
-        using var context = CreateTestContext();
-
-        var scriptPath = context.DeployScript("ForceProperties3.csx");
-        context.DotNetCliBuildTestSolution($"-p:Git2SemVer_ScriptPath={scriptPath}");
-        context.PackTestSolution();
-        VersioningBuildTestContext.AssertFileExists(context.PackageOutputDir, "NoeticTools.TestApplication.1.2.3-alpha.nupkg");
-
-        var output = DotNetProcessHelpers.RunDotnetApp(context.CompiledAppPath, context.Logger);
-        Assert.That(output, Does.Contain("""
-                                         Assembly version:       200.201.202.0
-                                         File version:           200.201.212
-                                         Informational version:  2.2.2-beta
-                                         Product version:        2.2.2-beta
-                                         """));
-    }
-
-    [Test]
-    [CancelAfter(60000)]
-    public void BuildOnlyTest()
-    {
-        using var context = CreateTestContext();
-
-        var scriptPath = context.DeployScript("ForceProperties3.csx");
-        context.DotNetCliBuildTestSolution($"-p:Git2SemVer_ScriptPath={scriptPath}");
-
-        var output = DotNetProcessHelpers.RunDotnetApp(context.CompiledAppPath, context.Logger);
-        Assert.That(output, Does.Contain("""
-                                         Assembly version:       200.201.202.0
-                                         File version:           200.201.212
-                                         Informational version:  2.2.2-beta
-                                         Product version:        2.2.2-beta
-                                         """));
-    }
-
-    [Test]
-    [CancelAfter(60000)]
-    public void PackWithForcingProperties1ScriptTest()
-    {
-        using var context = CreateTestContext();
-
-        var scriptPath = context.DeployScript("ForceProperties1.csx");
-
-        var result = context.DotNetCli.Pack(context.TestSolutionPath, context.BuildConfiguration, $"-p:Git2SemVer_ScriptPath={scriptPath} -fileLogger");
-        Assert.That(result.returnCode, Is.EqualTo(0), result.stdOutput);
-
-        var output = DotNetProcessHelpers.RunDotnetApp(context.CompiledAppPath, context.Logger);
-        Assert.That(output, Contains.Substring("""
-                                               Assembly version:       1.2.3.0
-                                               File version:           4.5.6
-                                               Informational version:  11.12.13-a-prerelease+metadata
-                                               Product version:        11.12.13-a-prerelease+metadata
-                                               """));
-        VersioningBuildTestContext.AssertFileExists(context.PackageOutputDir, "NoeticTools.TestApplication.5.6.7.nupkg");
     }
 }
