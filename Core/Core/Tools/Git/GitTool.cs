7bfb2a9fb002a86743c8398e7e8b53475ba5c673
Rob Smyth <RobSmyth@users.noreply.github.com>
2025-05-27 16:26:41 +1000

    dev: WIP

--- Core/Core/Tools/Git/GitTool.cs
+++ Core/Core/Tools/Git/GitTool.cs
@@ -213,7 +213,8 @@ public class GitTool : IGitTool
 
     private async Task<string> GetBranchNameAsync()
     {
-        var repository = new LibGit2Sharp.Repository(RepositoryDirectory/*@"C:\Sources\MyRepos\Git2SemVer.MSBuild.Fork"*/); //>>>
+        var repository = new LibGit2Sharp.Repository(RepositoryDirectory); //>>>
+        Console.WriteLine($"=== branch: {repository.Head.FriendlyName}");
 
         var stdOutput = await RunAsync("status -b -s --porcelain");
         return _gitResponseParser.ParseStatusResponseBranchName(stdOutput);
