a4dfee04488412dff04f577ee7ce434a61e24648
Rob Smyth <RobSmyth@users.noreply.github.com>
2025-05-28 14:52:38 +1000

    dev: WIP

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,8 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.approval
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
-import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetNugetPush
 import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetRestore
 import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetTest
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
@@ -10,8 +8,8 @@ import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnMetr
 import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnText
 import jetbrains.buildServer.configs.kotlin.failureConditions.failOnMetricChange
 import jetbrains.buildServer.configs.kotlin.failureConditions.failOnText
-import jetbrains.buildServer.configs.kotlin.projectFeatures.githubIssues
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -38,27 +36,10 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2025.03"
 
 project {
-    description = "MSBuild task to version projects"
 
-    buildType(DeployLocalTeamCityPackage)
+    vcsRoot(HttpsGithubComNoetictoolsGit2semverMsbuildRefsHeadsMain)
+
     buildType(BuildAndTest)
-
-    features {
-        githubIssues {
-            id = "PROJECT_EXT_11"
-            displayName = "NoeticTools/Git2SemVer.MSBuild"
-            repositoryURL = "https://github.com/NoeticTools/Git2SemVer.MSBuild"
-            authType = storedToken {
-                tokenId = "tc_token_id:CID_3de2c2727993edab40e4371046ac9db7:-1:9e7b82f7-5941-476b-ba54-211a20bbb5ca"
-            }
-        }
-    }
-
-    cleanup {
-        baseRule {
-            artifacts(builds = 20, days = 5)
-        }
-    }
 }
 
 object BuildAndTest : BuildType({
@@ -120,7 +101,7 @@ object BuildAndTest : BuildType({
     }
 
     failureConditions {
-        executionTimeoutMin = 2
+        executionTimeoutMin = 3
         failOnMetricChange {
             enabled = false
             metric = BuildFailureOnMetric.MetricType.TEST_COUNT
@@ -159,40 +140,16 @@ object BuildAndTest : BuildType({
     }
 })
 
-object DeployLocalTeamCityPackage : BuildType({
-    name = "Deploy (local TeamCity) - package"
-    description = "Deploy NuGet package"
-
-    enablePersonalBuilds = false
-    type = BuildTypeSettings.Type.DEPLOYMENT
-    buildNumberPattern = "%build.counter% (${BuildAndTest.depParamRefs.buildNumber})"
-    maxRunningBuilds = 1
-
-    steps {
-        dotnetNugetPush {
-            name = "Push NuGet package"
-            id = "Publish2"
-            packages = "NoeticTools.*.nupkg"
-            serverUrl = "http://10.1.10.78:8111/httpAuth/app/nuget/feed/_Root/TeamCity/v3/index.json"
-            apiKey = "credentialsJSON:bd18b974-1188-423d-9efd-8836806c3669"
-        }
-    }
-
-    features {
-        approval {
-            approvalRules = "user:robert"
-            manualRunsApproved = false
-        }
-    }
-
-    dependencies {
-        artifacts(BuildAndTest) {
-            buildRule = lastSuccessful("""
-                +:<default>
-                +:*
-            """.trimIndent())
-            cleanDestination = true
-            artifactRules = "+:NoeticTools.Git2SemVer.MSBuild.*.nupkg"
-        }
+object HttpsGithubComNoetictoolsGit2semverMsbuildRefsHeadsMain : GitVcsRoot({
+    name = "https://github.com/noetictools/git2semver.msbuild#refs/heads/main"
+    url = "git@github.com:NoeticTools/Git2SemVer.MSBuild.git"
+    branch = "refs/heads/main"
+    branchSpec = """
+        +:refs/heads/*
+        +:refs/tags/*
+    """.trimIndent()
+    useTagsAsBranches = true
+    authMethod = uploadedKey {
+        uploadedKey = "Git2SemVerMSBuildWriteSSH"
     }
 })
