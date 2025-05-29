31becf236a4e62f80d476d6080390de27f7eae68
Rob Smyth <RobSmyth@users.noreply.github.com>
2025-05-29 15:46:21 +1000

    dev: WIP

--- Core/Core/Tools/Git/IGitProcessCli.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NoeticTools.Git2SemVer.Core.Tools.Git;
-
-public interface IGitProcessCli
-{
-    string WorkingDirectory { get; set; }
-
-    Task<int> RunAsync(string commandLineArguments,
-                       TextWriter standardOut, TextWriter errorOut);
-}
